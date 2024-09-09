4978db9fdb630a2b7876c60ceda533ea87f6a61c
manu521 <emanoelecandido.15@gmail.com>
2024-09-09 14:44:13 -0300

    Create Captura de Tela (484).SQL

---
+++ Captura de Tela (484).SQL
@@ -0,0 +1,11 @@
+/*
+SENAC DF - CEP Jessé Freire 
+2024.07.276 - Aprendizagem Profissional de Qualificação em Desenvolvimento de Softwares 
+Prof° Hudson Neves 
+Projeto Final - Implementação de Banco de Dados Relacional com MySQL
+Projeto - Clinica Odontológica OdontoVida */
+
+-- 1. Criação do Arquivo/Banco de Dados
+create database OdontoVida;
+-- 2. Acessando  Arquivo/Banco de Dados 
+use OdontoVida;
